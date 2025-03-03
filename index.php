7c66aabaec300be9734b70f3722d0e48597e9b44
MerlinMaloi <malloialeks@gmail.com>
2025-03-04 01:36:08 +0200

    sf2

--- index.php
+++ index.php
@@ -54,6 +54,51 @@ function workDaysJaneDoe($dayw){
     </tr>
   </tbody>
 </table>
+<br>
+<?php
+$a1 = 0;
+$b1 = 0;
+
+for ($i = 0; $i <= 5; $i++) {
+   $a1 += 10;
+   echo $a1 . "\n";
+   $b1 += 5;
+   echo $b1 . "\n";
+}
+
+echo "End of the loop: a1 = $a1, b1 = $b1 \n";
+?><br>
+<?php
+$a2 = 0;
+$b2 = 0;
+$i = 0;
+
+while ($i <= 5) {
+   $a2 += 10;
+   echo $a2 . "\n";
+   $b2 += 5;
+   echo $b2 . "\n";
+   $i++ ;
+};
+
+echo "End of the loop: a2 = $a2, b2 = $b2 \n";
+?><br>
+<?php
+$a3 = 0;
+$b3 = 0;
+$i = 0;
+
+do {
+   $a3 += 10;
+   echo $a3 . "\n"; 
+   $b3 += 5;
+   echo $b3 . "\n"; 
+   $i ++ ;
+} while ($i <= 5);
+
+echo "End of the loop: a3 = $a3, b3 = $b3";
+
+?>
 </div>
 </body>
 </html>
